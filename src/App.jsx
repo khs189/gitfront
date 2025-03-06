02e0a846b47067d5472e00283cb008c6c036beeb
khs189 <khs189@naver.com>
2025-03-06 22:36:05 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SurveyForm from './pages/SurveyForm';
 import SurveyAnswer from './pages/SurveyAnswer';
@@ -6,14 +6,14 @@ import ManageSurvey from './pages/ManageSurvey';
 
 function App() {
   return (
-    <Router basename="/gitfront">
+    <BrowserRouter basename="/gitfront">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-survey" element={<SurveyForm />} />
         <Route path="/survey/:surveyName" element={<SurveyAnswer />} />
         <Route path="/manage-survey/:surveyName" element={<ManageSurvey />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
