e3cf545f817a5b26607fe660a2e76d25d49c0fae
khs189 <khs189@naver.com>
2025-04-04 21:05:17 +0900

    d
    d

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gitfront/',
   build: {
     assetsInlineLimit: 0, // 정적 파일을 직접 로드하도록 설정
   },
